802a188ac4cb491e5d465f70f55e653ed160171b
Alexis Dumon <alexisdumon@gmail.com>
2022-01-25 17:25:49 +0000

    starting to refactor (does not work)

--- agents.py
+++ agents.py
@@ -1,87 +1,165 @@
 from mesa import Agent
 import random
+import pandas as pd
+
 # from model import MarketModel
 
-# agents should have current states - depending on their states they should decide to do a particular action
-class ModelAgent(Agent):
+ethereumData = pd.read_csv('cleanedEuthereumData.csv')
 
-    def __init__(self, unique_id, model, currencyMarket):
-        super().__init__(unique_id, model)
-        self.currencyMarket = currencyMarket
-        self.currentObjective = None
+# should be updated at every turn
+marketIndicators = {
+                "ethereum" : { 'moving_average_1' : 10, 'moving_average_5' : 5, 'moving_average_10' : 2 },
+            }
 
-    def getWallet(self):
-        return self.wallet
-    
-    def step(self):
-        return 27
+class CurrencyMarket:
 
-
-class MarketAgent(ModelAgent):
-
-    def __init__(self, unique_id, model, currencyMarket):
-        super().__init__(unique_id, model, currencyMarket)
+    def __init__(self, listOfCurrencies):
+        self.marketIndicators = marketIndicators # hard coded as fuck 
+        self.currencies = listOfCurrencies
+        self.orderBook = OrderBook()
         
+    def getAvailableCurrencies(self):
+        return self.currencies
+    
+    def updateMarketIndicators(self, currency):
+        self.findMovingAverages(currency)
+    
+    def getMarketIndicators(self):
+        return self.marketIndicators
+
+    def getCurrentRound(self):
+        return self.model.round
+    
+    def getOrderBook(self):
+        return self.orderBook
+
+    # INDICATORS
+    def calcMovingAverage(self, currency, currentRound, spread):
+        """ spread: how many steps back do you want to calculate the moving average for """
+        total = 0
+        startRound = currentRound - spread
+        for round in range(startRound, currentRound):
+            total += currency.getPriceAtRound(round)
+        moving_average = total / spread
+        return moving_average
+
+class Currency:
+
+    def __init__(self, name, conversionSymbol, type, amountInCirculation, data):
+        self.name = name
+        self.conversionSymbol = conversionSymbol
+        self.type = type
+        self.amountInCirculation = amountInCirculation
+        self.data = data
+        self.transactions = 0
+
+    def getPriceAtRound(self, round):
+        return self.data[self.conversionSymbol].values[round]
+
+    def getData(self):
+        return self.data
+    
+    def getName(self):
+        return self.name
+    
+    def getType(self):
+        return self.type
+    
+    def getNumOfTransactions(self):
+        return self.transactions
+    
+    def addTransaction(self):
+        self.transactions += 1
+
+class Strategy:
+    def __init__(self):
+        self.order_id = 0
+        pass
+        
+    def getOrder(self, agent, round, availableCurrencies, amountOfCurrency):
+        """ returns a random order with random currency for a specific agent """
+        if not agent.hasMadeOrder:
+            self.order_id += 1
+            orderType = random.choice(['OPEN', 'CLOSE'])
+            chosenCurrency = random.choice(availableCurrencies)
+            return [orderType, chosenCurrency, round, self.order_id, agent.unique_id, amountOfCurrency] # creates an ORDER
+        else: return None
+
+class Order:
+    """ 
+        data structure defining order objects 
+        {   
+            "OPEN" :  {ethereum : { 1 : { agent_id : 1, amountOfCurrency : 2, timestep : 1 }, 2 : { agent_id : 1, amountOfCurrency : 2, timestep : 1 } }
+            "CLOSE" :  {ethereum : { 3 :  { agent_id : 1, amountOfCurrency : 2, timestep : 1 } } }
+        }
+    """
+    def __init__(self, orderType, currency, round, order_id, agent_id, amount ) -> None:
+        self.orderType = orderType
+        self.currency = currency
+        self.timestep = round
+        self.order_id = order_id
+        self.agent_id = agent_id
+        self.order = [self.orderType, self.currency, self.timestep, self.order_id, self.agent_id]
+
+    def getOrder(self):
+        return self.order
+        
+class OrderBook:
+    """ 
+        an order book is a data structure that aims to facilitate matching SELLERS and BUYERS of the different currencies
+        Orderbook holds multiple Order objects
+    """
+    def __init__(self) -> None:
+        self.orders = {"OPEN" : {}, "CLOSE" : {}}
+
+    """" wtf am i trying to do ???? """
+    def appendOrderBook(self, order):
+        if order[0] == "OPEN":
+            if order[1] in self.orders["OPEN"].keys():
+                self.orders["OPEN"][order[1]][order.order_id] = order
+            else:
+                self.orders["OPEN"][order[1]] = {order.order_id}
+                self.orders["OPEN"][order[1]][order.order_id] = order 
+
+    def getOrders(self):
+        return self.orders
+
+
+class MarketAgent:
+
+    def __init__(self, unique_id, strategy, model, currencyMarket, wallet):
+        self.unique_id = unique_id
         self.model = model
-        self.currentObjective = None
-        self.setCurrentObjective()
-        
-        """ set everyone off with a fixed amount in their wallets """
-        self.wallet = self.createWallet()
+        self.currencyMarket = currencyMarket
 
-        self.amountOfGoods = random.randint(0, 2)
+        # randomly initialised fields
+        self.strategy = strategy
+        self.hasMadeOrder = False    # this one can remain unrandomised for a bit    
+        self.wallet = wallet
     
-
     def createWallet(self):
         wallet = {}
-        for currency in self.currencyMarket.getAvailableCurrencies():
-            wallet[currency.getName()] = 2.7 # for now fixed amount of both currencies!!!
-
+        for currency in self.currencyMarket.getAvailableCurrencies().getName():
+            wallet[currency] = 2.7 # for now fixed amount of both currencies!!!
         return wallet
 
-    def setCurrentObjective(self):
-        """ every round market agents are randomly assigned a new current object BUY or SELL """
-        self.currentObjective = random.choice(['BUY', 'SELL'])
-        # print (self.currentObjective)
-
-    def chooseCurrencyForTransaction(self):
-        """ depending on current objective agent will want to buy a good or sell a good """
-        # get data for the different currencies INSTEAD OF HAVING each agent calculate make CURRENCYmARKET calculate 
-        # have agents QUERY for the processed data
-
-        # ASSUME best currency for BUYER is best for SELLER
-        return None
-
-    def hasEnoughOfCurrency(self, currency):
-        return self.wallet[currency.getName()] > 1
-
-
     # what happens during one round of the simulation for one agent
+    ## a limited amount of agents get to perform their step actions per turn 
     def step(self):
-        """ for now it chooses a random agent and least fluctuating currency, and goes ahead with the transaction only
-        buyers """
+        """ if it was chosen as a trading agent this turn; then it adds an order to the order book list """
 
-        if self.currentObjective == "BUY" and len(self.model.sellers) > 0: # if agent's objective is to BUY and there are available SELLERS
-            other = self.model.sellers[0] # choose a random seller agent
-            self.model.sellers = self.model.sellers[1:] # remove it from the list of available
-            
-            currency = self.currencyMarket.getLeastFluctuatingCurrency() # chooses least fluctuating currency
-            
-            chosenCurrency = self.currencyMarket.getProbabilisticCurrencyChoice(currency)
-            if self.hasEnoughOfCurrency(chosenCurrency) and other.amountOfGoods >= 1: # if agent has enough of the currency and other has enough goods
-                # do the exchange using the chosen currency
-                currencyName = chosenCurrency.getName()
-                self.wallet[currencyName] -= 1
-                self.amountOfGoods -= 1
+        newOrder = self.strategy.getOrder(self, self.model.round, list(self.wallet.keys()), 27)
 
-                other.wallet[currencyName] += 1
-                other.amountOfGoods += 1
-                
-                chosenCurrency.addTransaction()
-
-        # after finished step find new objective // could put this in model's step method... 
-        self.setCurrentObjective()
-
+        self.hasMadeOrder = True
+        self.currencyMarket.getOrderBook().appendOrderBook(newOrder)
         
 
+# listOfDataCurrencies = []
+# ethereum = Currency('ethereum', "USD/ETH", "crypto", 100, ethereumData)
+# listOfDataCurrencies.append(ethereum)
+# currencyMarket = CurrencyMarket(listOfDataCurrencies)
 
+# agent1 = MarketAgent(1, Strategy(), MarketModel, currencyMarket, {'ethereum' : 27})
+# agent2 = MarketAgent(2, Strategy(), MarketModel, currencyMarket, {'ethereum' : 27})
+# agent1.step()
+# agent2.step()
--- model.py
+++ model.py
@@ -4,14 +4,14 @@ from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
 from numpy import Infinity
 import pandas as pd
-from pandas.io.formats.format import TextAdjustment
-from tornado.autoreload import _reload_on_update
-from agents import ModelAgent, MarketAgent
+from agents import MarketAgent, Currency, CurrencyMarket
 import random
 import copy
 
-def getNumberOfTetherTransactions(model) :
-    return model.listOfCurrencies[1].getNumOfTransactions()
+from agents import Strategy
+
+# def getNumberOfTetherTransactions(model) :
+#     return model.listOfCurrencies[1].getNumOfTransactions()
 
 def getNumberOfEthereumTransactions(model) :
     return model.listOfCurrencies[0].getNumOfTransactions()
@@ -31,22 +31,19 @@ def getEthereumPrice(model):
 ethereumData = pd.read_csv('cleanedEuthereumData.csv')
 tetherData = pd.read_csv('cleanedTetherData.csv')
 
-listOfDataOfCurrencies = []
-listOfDataOfCurrencies.append(ethereumData)
-listOfDataOfCurrencies.append(tetherData)
+# listOfDataOfCurrencies = []
+# listOfDataOfCurrencies.append(ethereumData)
 
 class MarketModel(Model):
     def __init__(self, num_agents = 10):
         self.round = 0 # what round we are at in the simulation
 
         ethereum = Currency("ethereum", "USD/ETH", "crypto", 100, ethereumData)
-        tether = Currency("tether", "USD/USDT", "fiat-backed", 100, tetherData)
 
         self.listOfCurrencies = []
         self.listOfCurrencies.append(ethereum)
-        self.listOfCurrencies.append(tether)
 
-        self.currencyMarket = CurrencyMarket(self, self.listOfCurrencies)
+        self.currencyMarket = CurrencyMarket(self.listOfCurrencies)
 
         self.schedule = RandomActivation(self)
 
@@ -54,11 +51,10 @@ class MarketModel(Model):
 
         self.datacollector = DataCollector(
             model_reporters = {
-                                "num_of_transactions" : totalTransactions,
-                                "num_of_tether_transactions": getNumberOfTetherTransactions,
-                                "num_of_ethereum_transactions": getNumberOfEthereumTransactions,
-                                "tether_price": getTetherPrice,
-                                "ethereum_price": getEthereumPrice,
+                                # "num_of_transactions" : totalTransactions,
+                                # "num_of_ethereum_transactions": getNumberOfEthereumTransactions,
+                                # "tether_price": getTetherPrice,
+                                # "ethereum_price": getEthereumPrice,
                             },
             agent_reporters= {
                 "Wallets" : "wallet"
@@ -68,7 +64,7 @@ class MarketModel(Model):
         self.sellers = None
         self.buyers = None
 
-        self.agents = []
+        self.agents = {}
 
         self.createAgents(num_agents)
     
@@ -77,98 +73,27 @@ class MarketModel(Model):
         self.datacollector.collect(self)
 
     def createAgents(self, num_agents):
+        strategy = Strategy()
+        wallet = { "ethereum" : 27 }
         for i in range(self.num_agents): 
-            a = MarketAgent(i, self, self.currencyMarket) # does nothing for now... 
+            a = MarketAgent(i, strategy, self, self.currencyMarket, wallet) # does nothing for now... 
 
             self.schedule.add(a)
-            self.agents.append(a)
+            self.agents[i] = a
 
     def step(self):
         self.round += 1
-
-        self.sellers = [x for x in self.agents if x.currentObjective == "SELL"]
-        self.buyers = [x for x in self.agents if x.currentObjective == "BUY"]
-        self.currencyMarket.findLeastFluctuatingCurrency(self.round)
         
         self.schedule.step() # runs the step method for all Agents
 
+        orders = self.currencyMarket.getOrderBook().getOrders()
+        print (orders)
         self.datacollector.collect(self)
         print ("-------- A step has happened -------------")
 
 # --------------------------------------------------------------------------
 
 
-class CurrencyMarket:
-
-    def __init__(self, model, listOfCurrencies):
-        self.model = model
-        self.currencies = listOfCurrencies
-        self.leastFluctuatingCurrency = None
-        self.findLeastFluctuatingCurrency(0)
-
-    def getAvailableCurrencies(self):
-        return self.currencies
-    
-    # will need to be refactored AF (does two things )
-    def getLeastFluctuatingCurrency(self):
-        """ returns currency object """
-        return self.leastFluctuatingCurrency
-
-    # REFACTOR
-    def getProbabilisticCurrencyChoice(self, currency):
-        indices = []
-        index = self.currencies.index(currency)
-        for i in range(len(self.currencies)):
-            if (index == i):
-                indices.append(i)
-                indices.append(i)
-            indices.append(i)
-        randomIndex = random.choice(indices)
-        return self.currencies[randomIndex]
-
-
-    def findLeastFluctuatingCurrency(self, round):
-        smallestDiff = Infinity
-        for currency in self.currencies:
-            initPrice = currency.getPriceAtRound(0)
-            currPrice = currency.getPriceAtRound(round)
-            diff = abs(currPrice - initPrice)
-            if (diff < smallestDiff):
-                self.leastFluctuatingCurrency = currency
-                smallestDiff = diff
-        
-    def getCurrentRound(self):
-        return self.model.round
-
-class Currency:
-
-    def __init__(self, name, conversionSymbol, type, amountInCirculation, data):
-        self.name = name
-        self.conversionSymbol = conversionSymbol
-        self.type = type
-        self.amountInCirculation = amountInCirculation
-        self.data = data
-        self.transactions = 0
-
-    def getPriceAtRound(self, round):
-        return self.data[self.conversionSymbol].values[round]
-
-    def getData(self):
-        return self.data
-    
-    def getName(self):
-        return self.name
-    
-    def getType(self):
-        return self.type
-    
-    def getNumOfTransactions(self):
-        return self.transactions
-    
-    def addTransaction(self):
-        self.transactions += 1
-
-
 model = MarketModel(10)
-for i in range(5):
+for i in range(1):
     model.step()
