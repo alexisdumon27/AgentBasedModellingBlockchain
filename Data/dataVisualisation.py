1bc3c70b2ca4044fe69b870e0bd0bb08a86319e7
Alexis Dumon <alexisdumon@gmail.com>
2022-03-29 18:05:27 +0100

    forget to add this one

--- Data/dataVisualisation.py
+++ Data/dataVisualisation.py
@@ -62,24 +62,24 @@ symbols = ["ETH/USDT", "USDT/ETH",
 #     print(i,") ", symbols[i], ": ", percentage_change)
 
 ### maximum MIN-MAX Difference First Two MONTHS
-# for i in range(12):
-#     new_index = exchange_rates.index[exchange_rates['date'] == "2020-02-28 23:54:00"].tolist()
-#     a = exchange_rates[symbols[i]].iloc[0:new_index[0]]
-#     vmin = a[0]
-#     dmax = 0
-#     for j in range(len(a)):
-#         if (a[j] < vmin):
-#             vmin = a[j]
-#         elif (a[j] - vmin > dmax):
-#             dmax = a[j] - vmin
-#             percentage_change = (dmax / vmin) * 100
-#     print(i,") ", symbols[i], ": ", percentage_change, "diff: ", dmax)
+for i in range(12):
+    new_index = exchange_rates.index[exchange_rates['date'] == "2020-01-14 23:00:00"].tolist()
+    a = exchange_rates[symbols[i]].iloc[0:new_index[0]]
+    vmin = a[0]
+    dmax = 0
+    for j in range(len(a)):
+        if (a[j] < vmin):
+            vmin = a[j]
+        elif (a[j] - vmin > dmax):
+            dmax = a[j] - vmin
+            percentage_change = (dmax / vmin) * 100
+    print(i,") ", symbols[i], ": ", percentage_change, "diff: ", dmax)
 
-###### First Two months Percent change = ((new - old) / old) * 100
+###### First Two weeks Percent change = ((new - old) / old) * 100
 # for i in range(12):
 #     old = exchange_rates[symbols[i]].iloc[0]
 #     # find index where date = "2020-02-28 23:54:00"
-#     new_index = exchange_rates.index[exchange_rates['date'] == "2020-02-28 23:54:00"].tolist()
+#     new_index = exchange_rates.index[exchange_rates['date'] == "2020-01-14 23:00:00"].tolist()
 #     new = exchange_rates[symbols[i]].iloc[new_index[0]]
 #     percent_change = ((new - old) / old) * 100
 #     print (i,") ", symbols[i], ": ", percent_change, "%", ", old:", old, ", new:", new) 
@@ -95,23 +95,46 @@ symbols = ["ETH/USDT", "USDT/ETH",
 #             total += 1
 #     print(i,") ", symbols[i], ": ", total, ", length: ", len(data))
 
+###### X / US Dollar graph
+# _25_tick = round(exchange_rates.shape[0] * 0.25),
+# _50_tick = round(exchange_rates.shape[0] * 0.5),
+# _75_tick = round(exchange_rates.shape[0] * 0.75),
+# plt.xticks([
+#         0, 
+#         _25_tick[0],
+#         _50_tick[0],
+#         _75_tick[0],
+#         exchange_rates.shape[0]
+#     ],
+#     [   
+#         exchange_rates["date"].iloc[0].split(" ")[0],
+#         exchange_rates["date"].iloc[_25_tick[0]].split(" ")[0],
+#         exchange_rates["date"].iloc[_50_tick[0]].split(" ")[0],
+#         exchange_rates["date"].iloc[_75_tick[0]].split(" ")[0],
+#         exchange_rates["date"].iloc[-1].split(" ")[0]
+#     ],
+#     visible=True, rotation="horizontal")
+# plt.plot(range(len(exchange_rates["date"])), exchange_rates["BNB/USD"]) 
+# plt.title('BNB/USD')
+# plt.xlabel("Time in Days")
+# plt.ylabel("BNB/USD price level")
+# plt.show()
 
-
-###### number of positive hours (two months)
-for i in range(12):
-    end_index = exchange_rates.index[exchange_rates['date'] == "2020-02-28 23:54:00"].tolist()
-    data = exchange_rates[symbols[i]].iloc[0:end_index[0]]
-    total = 0
-    for j in range(len(data)):
-        if j + 1 == len(data):
-            break
-        if data.iloc[j] < data.iloc[j+1]:
-            total += 1
-    print(i,") ", symbols[i], ": ", total, ", length: ", len(data))
-
-### Difference between min and max (must be min left max right) FIRST TWO MONTHS
+###### number of positive hours (two weeks)
 # for i in range(12):
-#     end_index = exchange_rates.index[exchange_rates['date'] == "2020-02-28 23:54:00"].tolist()
+#     end_index = exchange_rates.index[exchange_rates['date'] == "2020-01-14 23:00:00"].tolist()
+#     data = exchange_rates[symbols[i]].iloc[0:end_index[0]]
+#     total = 0
+#     for j in range(len(data)):
+#         if j + 1 == len(data):
+#             break
+#         if data.iloc[j] < data.iloc[j+1]:
+#             total += 1
+#     print(i,") ", symbols[i], ": ", total, ", length: ", len(data))
+
+### Difference between min and max FIRST TWO WEEKS
+# for i in range(12):
+#     end_index = exchange_rates.index[exchange_rates['date'] == "2020-01-14 23:00:00"].tolist()
 #     data = exchange_rates[symbols[i]].iloc[0:end_index[0]]
 #     min_index = data.idxmin()
 #     max_index = data.idxmax()
@@ -136,10 +159,10 @@ for i in range(12):
 #     print (i,") ", symbols[i], ": ", std_dev) 
 
 
-#### Standard deviation first two months
+#### Standard deviation first two weeks
 # for i in range(12):
 #     # normalise
-#     new_index = exchange_rates.index[exchange_rates['date'] == "2020-02-28 23:54:00"].tolist()
+#     new_index = exchange_rates.index[exchange_rates['date'] == "2020-01-14 23:00:00"].tolist()
 #     data = exchange_rates[symbols[i]].iloc[0:new_index[0]]
 #     norm = (data - data.mean()) / (data.max() - data.min())
 #     std_dev = norm.std()
