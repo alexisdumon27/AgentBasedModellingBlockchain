e67ad24a80154013d6060007e6483c5a9b944189
Alexis Dumon <alexisdumon@gmail.com>
2022-04-07 14:34:21 +0100

    starts to change the file structure

---
+++ Model/agents.py
@@ -0,0 +1,215 @@
+from cmath import inf
+import random
+from mesa import Agent
+
+class MarketAgent(Agent):
+
+    def __init__(self, unique_id, model, strategy, risk_level, currency_market):
+        self.unique_id = unique_id
+        self.model = model
+        self.round = model.round
+        self.currency_market = currency_market
+        self.risk_level = risk_level
+        self.strategy = strategy
+
+        self.has_made_open_order = False
+        self.has_made_closing_order = False
+        self.open_transaction_was_successfull = False
+        self.closing_transaction_was_successfull = False
+
+        self.current_investment = {"amount": 0, "bought_currency": None, "sold_currency": None} # an dictionary holding info about current Investment Object
+        self.current_order = None
+
+        self.wallet = {}
+        if self.strategy.name == "random":
+            self.createWalletRandom()
+        else:
+            self.createWallet()
+            self.initialUSDValueOfWallet = self.getUSDWalletValue()
+            self.currentUSDValueOfWallet = self.getUSDWalletValue()
+            self.currentUSDValueOfGains = 0
+            self.open_orders = []
+            self.close_orders = []
+    
+    # start with $100 worth of all possible currencies
+    def createWallet(self):
+        # choose 2 currencies and put $100 worth for each
+        starting_amount_based_on_risk_level = {"averse": 100, "neutral": 270, "taker": 500}
+        i = 0
+        for currency in random.sample(self.currency_market.getAvailableCurrencies(), len(self.currency_market.getAvailableCurrencies())):
+            if i == 0 or i == 1:
+            # give 100 $ worth to 2 out of all currencies
+                price_curr = currency.getPriceAtRound(self.round)
+                self.wallet[currency] = starting_amount_based_on_risk_level[self.risk_level] / price_curr # start with 100 of both currencies
+            else: self.wallet[currency] = 0
+            i += 1
+
+    def createWalletRandom(self):
+        for currency in self.currency_market.getAvailableCurrencies():
+            self.wallet[currency] = float(inf)
+
+    # what happens during one round of the simulation for one agent
+    ## a limited amount of agents get to perform their step actions per turn 
+    def step(self):
+        """ if it was chosen as a trading agent this turn; then it adds an order to the order book list """
+        self.round = self.model.round
+        if self.strategy.name == "random":
+            self.randomAgentStep()
+        else:
+            self.strategyAgentStep()
+    
+    def randomAgentStep(self):
+        """
+            1. initial -- nothing --> make an open order with probability 0.5
+            2. has an order in the orderbook --> wait until expiration time
+            3. a transaction has happened --> reinitialise
+        """
+        if self.current_order == None:
+            if random.random() < 0.5:
+                self.makeOrder("OPEN")
+                self.has_made_open_order = True
+        elif self.has_made_open_order and not self.open_transaction_was_successfull:
+            if self.current_order.expiration_time > 0:
+                self.current_order.expiration_time -= 1
+            else:
+                self.currency_market.order_book.removeAgentOrder(self)
+                self.has_made_open_order = False
+                self.current_order = None
+        elif self.has_made_open_order and self.open_transaction_was_successfull:
+            self.initialiseParameters()
+            
+    def strategyAgentStep(self):
+        if not self.has_made_open_order and not self.has_made_closing_order and not self.has_made_closing_order and not self.closing_transaction_was_successfull:
+            # check if it should make an order
+            # Following their strategy should it make an order?
+            self.makeOrder("OPEN") # current_order is also updated
+            if self.hasACurrentOrder():
+                self.has_made_open_order = True
+        elif self.has_made_open_order and not self.open_transaction_was_successfull and not self.has_made_closing_order and not self.closing_transaction_was_successfull:
+            # wait for open order to be successful (Open order not fulfilled yet ... )
+            if self.current_order.expiration_time > 0:
+                self.current_order.expiration_time -= 1
+            else: 
+                if self.strategy.name == "random":
+                    # re-initialise try another order
+                    # remove previous order from order book
+                    self.currency_market.order_book.removeAgentOrder(self)
+                    self.has_made_open_order = False
+                    self.current_order = None
+                else:
+                    # UPDATE the limit_price of the order
+                    self.updateCurrentOrderLimitPrice() # updates for agent
+                    self.currency_market.order_book.updateAgentOrderLimitPriceInOrderBook(self, self.current_order.limit_price) # updates the orderbook with the agent's new limit price
+        elif self.has_made_open_order and self.open_transaction_was_successfull and not self.has_made_closing_order and not self.closing_transaction_was_successfull:
+            if self.strategy.closingConditionMet(self, self.round):
+                self.makeOrder("CLOSE") # current_order is also updated 
+                self.has_made_closing_order = True
+        elif self.has_made_open_order and self.open_transaction_was_successfull and self.has_made_closing_order and not self.closing_transaction_was_successfull:
+            # wait for it for open order to be successful
+            if self.current_order.expiration_time > 0:
+                self.current_order.expiration_time -= 1
+            else: 
+                # UPDATE the limit_price of the order
+                self.updateCurrentOrderLimitPrice() # updates for agent
+                self.currency_market.order_book.updateAgentOrderLimitPriceInOrderBook(self, self.current_order.limit_price) # updates the orderbook with agents new limit price
+        elif self.has_made_open_order and self.open_transaction_was_successfull and self.has_made_closing_order and self.closing_transaction_was_successfull:
+            # if agent has openned order, done open transac, closed order, and done closing transac
+            self.initialiseParameters()
+        
+        self.currentUSDValueOfWallet = self.getUSDWalletValue() # gets updated every round
+        self.currentUSDValueOfGains = self.currentUSDValueOfWallet - self.initialUSDValueOfWallet
+
+    def hasACurrentInvestment(self):
+        if self.current_investment == {"amount": 0, "bought_currency": None, "sold_currency": None}:
+            return False
+        else: True
+
+    def hasACurrentOrder(self):
+        return self.current_order != None
+
+    def initialiseParameters(self):
+        self.has_made_open_order = False
+        self.has_made_closing_order = False
+        self.open_transaction_was_successfull = False
+        self.closing_transaction_was_successfull = False
+        self.current_order = None
+        self.current_investment = {"amount": 0, "bought_currency": None, "sold_currency":None}
+
+    def makeOrder(self, order_type):
+        possible_order = None
+        if order_type == "OPEN":
+            possible_order = self.strategy.tryToMakeOpenOrder(self, self.round)
+        elif order_type == "CLOSE":
+            possible_order = self.strategy.makeCloseOrder(self, self.round)
+        
+        # was it a good time to make an order?
+        if possible_order != None:
+
+            self.current_order = possible_order
+            self.currency_market.getOrderBook().addOrder(self.current_order)
+            if self.strategy.name != "random":
+                buy_currency = self.current_order.buy_currency
+                sell_currency = self.current_order.sell_currency
+                symbol = buy_currency.symbol + "/" + sell_currency.symbol
+
+                self.currency_market.num_of_orders_by_currency_pairs[symbol][-1] += 1
+                self.currency_market.num_of_orders_dict_by_strategy_by_currency[self.strategy.name][symbol][-1] += 1
+
+                if order_type == "OPEN":
+                    self.open_orders.append([self.round, buy_currency.symbol, sell_currency.symbol, self.current_order.amount_of_buying_currency])
+                else:  
+                    self.close_orders.append([self.round, buy_currency.symbol, sell_currency.symbol, self.current_order.amount_of_buying_currency])
+
+
+    def getCurrenciesInWalletWithPositiveBalance(self):
+        currency_balance_above_zero = []
+        for currency_key in self.wallet.keys():
+            if self.wallet[currency_key] > 0:
+                currency_balance_above_zero.append(currency_key)
+        return random.sample(currency_balance_above_zero, len(currency_balance_above_zero))
+
+    def updateWallet(self, bought_currency, sold_currency, bought_currency_amount, sold_currency_amount):
+        self.wallet[bought_currency] += bought_currency_amount
+        self.wallet[sold_currency] -= sold_currency_amount
+    
+    def updateCurrentOrderAmount(self, amount):
+        self.current_order.amount_of_buying_currency -= amount
+
+    def updateCurrentOrderLimitPrice(self):
+        """
+            change to use the new limit price || remove order
+        """
+        buy_currency = self.current_order.buy_currency
+        sell_currency = self.current_order.sell_currency
+        symbol = buy_currency.symbol + "/" + sell_currency.symbol
+        current_exchange_rate = self.currency_market.getCurrenciesExchangeRate(symbol, self.model.round)
+        self.current_order.limit_price = self.strategy.getLimitPrice(current_exchange_rate) # update the limit_price !!!!
+
+    def updateCurrentInvestment(self, amount_sold, order):
+        self.current_investment["amount"] += amount_sold # amount of currency it sold to buy desired currency
+        self.current_investment["bought_currency"] = order[1][1]
+        self.current_investment["sold_currency"] = order[1][2]
+
+    def updateOrderStatus(self, type):
+        if type == "OPEN":
+            self.open_transaction_was_successfull = True
+        else: 
+            self.closing_transaction_was_successfull = True
+
+    def getUSDWalletValue(self):
+        total = 0
+        for currency in self.wallet:
+            amount_of_curr = self.wallet[currency]
+            price_curr = currency.getPriceAtRound(self.round)
+            total += amount_of_curr * price_curr
+        return total
+
+    def updateCurrentState(self, order, bought_currency_amount, sold_currency_amount, order_status = None, current_order_amount = None):
+        bought_currency = order[1][1]
+        sold_currency = order[1][2]
+
+        self.updateWallet(bought_currency, sold_currency, bought_currency_amount, sold_currency_amount)
+        self.updateCurrentInvestment(sold_currency_amount, order) # adds amount it had to sell to buy == amount bought by other_agent
+
+        if order_status != None: self.updateOrderStatus(order_status)
+        if current_order_amount != None: self.updateCurrentOrderAmount(bought_currency_amount)
---
+++ Model/model.py
@@ -0,0 +1,187 @@
+import random
+from mesa import Model
+from mesa.time import RandomActivation
+from mesa.datacollection import DataCollector
+import pandas as pd
+from Model.investment_strategies import MACDStrategy, MovingAverageStrategy, PivotPointStrategy, RSIStrategy, Strategy, RandomStrategy
+from Model.agents import MarketAgent
+from Model.currency_market import CurrencyMarket
+from Visualisation.dataVisualisationMethods import *
+
+class MarketModel(Model):
+    def __init__(self, starting_date = 1, ratio_of_random_strategy_to_other = 0.5, ratio_of_agents_engaged_each_turn = 0.5, num_agents = 10):
+        self.starting_date = starting_date
+        self.round = starting_date # index keeping count of the round of simulation
+        self.num_of_agents = num_agents
+        self.ratio_of_agents_engaged_each_turn = ratio_of_agents_engaged_each_turn
+        self.ratio_of_random_strategy_to_other = ratio_of_random_strategy_to_other
+        
+        self.exchange_rates = pd.read_csv('Data/exchange_rates.csv')
+
+        self.currency_market = CurrencyMarket(self.exchange_rates)
+
+        self.schedule = RandomActivation(self)
+
+        self.list_of_agents_not_including_random = []
+        self.createAgents()
+
+        self.datacollector = DataCollector(
+            model_reporters={
+                "num_transactions_total" : getTotalTransactions,
+                "num_transactions_ETH/USDT:USDT/ETH" : getTotalETH_USDTTransactions,
+                "num_transactions_ETH/BNB:BNB/ETH" : getTotalETH_BNBTransactions,
+                "num_transactions_ETH/BTC:BTC/ETH" : getTotalETH_BTCTransactions,
+                "num_transactions_BNB/BTC:BTC/BNB" : getTotalBNB_BTCTransactions,
+                "num_transactions_BNB/USDT:USDT/BNB" : getTotalBNB_USDTTransactions,
+                "num_transactions_BTC/USDT:USDT/BTC" : getTotalBTC_USDTTransactions,
+                "Pivot Point": getRelativeTotalWealthPivotPoint,
+                "Moving Average": getRelativeTotalWealthMovingAverage,
+                "RSI": getRelativeTotalWealthRSI,
+                "MACD": getRelativeTotalWealthMACD,
+                "Wealthy 0": getWealthiestAgents0,
+                "Wealthy 1": getWealthiestAgents1,
+                "Wealthy 2": getWealthiestAgents2,
+                "Wealthy 3": getWealthiestAgents3,
+                "Wealthy 4": getWealthiestAgents4,
+                "Wealthy 5": getWealthiestAgents5,
+                "Wealthy 6": getWealthiestAgents6,
+                "Wealthy 7": getWealthiestAgents7,
+                "Wealthy 8": getWealthiestAgents8,
+                "Wealthy 9": getWealthiestAgents9,
+                "Random orderbook" : getNumRandomOrderbook,
+                "Pivot Point orderbook": getNumPivotPointOrderbook,
+                "Moving Average orderbook": getNumMovingAverageOrderbook,
+                "RSI orderbook": getNumRSIOrderbook,
+                "MACD orderbook": getNumMACDOrderbook,
+                "num_transactions_pivot_point_ETH/USDT:USDT/ETH": getNumOfTransactionsPivotPointETHUSDT,
+                "num_transactions_pivot_point_ETH/BNB:BNB/ETH": getNumOfTransactionsPivotPointETHBNB,
+                "num_transactions_pivot_point_ETH/BTC:BTC/ETH": getNumOfTransactionsPivotPointETHBTC,
+                "num_transactions_pivot_point_BNB/BTC:BTC/BNB": getNumOfTransactionsPivotPointBNBBTC,
+                "num_transactions_pivot_point_BNB/USDT:USDT/BNB": getNumOfTransactionsPivotPointBNBUSDT,
+                "num_transactions_pivot_point_BTC/USDT:USDT/BTC": getNumOfTransactionsPivotPointBTCUSDT,   
+                "num_transactions_moving_average_ETH/USDT:USDT/ETH": getNumOfTransactionsMovingAverageETHUSDT,
+                "num_transactions_moving_average_ETH/BNB:BNB/ETH": getNumOfTransactionsMovingAverageETHBNB,
+                "num_transactions_moving_average_ETH/BTC:BTC/ETH": getNumOfTransactionsMovingAverageETHBTC,
+                "num_transactions_moving_average_BNB/BTC:BTC/BNB": getNumOfTransactionsMovingAverageBNBBTC,
+                "num_transactions_moving_average_BNB/USDT:USDT/BNB": getNumOfTransactionsMovingAverageBNBUSDT,
+                "num_transactions_moving_average_BTC/USDT:USDT/BTC": getNumOfTransactionsMovingAverageBTCUSDT,
+                "num_transactions_rsi_ETH/USDT:USDT/ETH": getNumOfTransactionsRSIETHUSDT,
+                "num_transactions_rsi_ETH/BNB:BNB/ETH": getNumOfTransactionsRSIETHBNB,
+                "num_transactions_rsi_ETH/BTC:BTC/ETH": getNumOfTransactionsRSIETHBTC,
+                "num_transactions_rsi_BNB/BTC:BTC/BNB": getNumOfTransactionsRSIBNBBTC,
+                "num_transactions_rsi_BNB/USDT:USDT/BNB": getNumOfTransactionsRSIBNBUSDT,
+                "num_transactions_rsi_BTC/USDT:USDT/BTC": getNumOfTransactionsRSIBTCUSDT,   
+                "num_transactions_macd_ETH/USDT:USDT/ETH": getNumOfTransactionsMACDETHUSDT,
+                "num_transactions_macd_ETH/BNB:BNB/ETH": getNumOfTransactionsMACDETHBNB,
+                "num_transactions_macd_ETH/BTC:BTC/ETH": getNumOfTransactionsMACDETHBTC,
+                "num_transactions_macd_BNB/BTC:BTC/BNB": getNumOfTransactionsMACDBNBBTC,
+                "num_transactions_macd_BNB/USDT:USDT/BNB": getNumOfTransactionsMACDBNBUSDT,
+                "num_transactions_macd_BTC/USDT:USDT/BTC": getNumOfTransactionsMACDBTCUSDT,       
+            }
+        )
+
+        self.running = True
+
+    def createAgents(self):
+        random_strategy = RandomStrategy("random", self.exchange_rates)
+        pivot_point_strategy = PivotPointStrategy("pivot_point", self.exchange_rates)
+        moving_average_strategy = MovingAverageStrategy("moving_average", self.exchange_rates)
+        macd_strategy = MACDStrategy('macd', self.exchange_rates)
+        rsi_strategy = RSIStrategy("rsi", self.exchange_rates)
+
+        risk_levels = ["averse", "neutral", "taker"]
+
+        # list of agents using each strategy
+        self.agents_using_each_strategy_dict = {
+            "random" : [],
+            "pivot_point" : [],
+            "moving_average" : [],
+            "macd" : [],
+            "rsi": []
+        }
+        num_of_random_agents = self.num_of_agents * self.ratio_of_random_strategy_to_other
+        agent_number = 0
+        while agent_number < self.num_of_agents: 
+            risk_level = random.choice(risk_levels)
+            a = None
+            if agent_number < num_of_random_agents:
+                a = MarketAgent(agent_number, self, random_strategy, risk_level, self.currency_market)
+                self.agents_using_each_strategy_dict["random"].append(a)
+            else:
+                if agent_number % 4 == 0:
+                    a = MarketAgent(agent_number, self, pivot_point_strategy, risk_level, self.currency_market)
+                    self.agents_using_each_strategy_dict["pivot_point"].append(a)
+                elif agent_number % 4 == 1:
+                    a = MarketAgent(agent_number, self, moving_average_strategy, risk_level, self.currency_market)
+                    self.agents_using_each_strategy_dict["moving_average"].append(a)
+                elif agent_number % 4 == 2:
+                    a = MarketAgent(agent_number, self, macd_strategy, risk_level, self.currency_market)
+                    self.agents_using_each_strategy_dict["macd"].append(a)
+                elif agent_number % 4 == 3:
+                    a = MarketAgent(agent_number, self, rsi_strategy, risk_level, self.currency_market)
+                    self.agents_using_each_strategy_dict["rsi"].append(a)
+                self.list_of_agents_not_including_random.append(a)
+                
+            self.schedule.add(a)
+            agent_number += 1
+
+    def step(self):
+
+        self.addNewRoundToDataCollector()
+
+        num_of_agents_per_turn = round(self.num_of_agents * self.ratio_of_agents_engaged_each_turn)
+        for i in range(num_of_agents_per_turn):
+            self.schedule.step() # runs the step method for all Agents
+
+        self.currency_market.order_book.sortOrdersInOrderBook()
+
+        self.currency_market.priceClearingMechanism() # do all transactions
+
+        self.datacollector.collect(self)
+        self.round += 1 # go to the next round
+
+    
+    def addNewRoundToDataCollector(self):
+        for key in self.currency_market.num_of_orders_by_currency_pairs:
+            array = self.currency_market.num_of_orders_by_currency_pairs[key]
+            if len(array) == 0:
+                array.append(0)
+            else: array.append(array[-1])
+        
+        for strategy_key in self.currency_market.num_of_orders_dict_by_strategy_by_currency:
+            strategy_values = self.currency_market.num_of_orders_dict_by_strategy_by_currency[strategy_key]
+            for symbol_key in strategy_values:
+                array = strategy_values[symbol_key]
+                if len(array) == 0:
+                    array.append(0)
+                else: array.append(array[-1])
+
+    # Called in server.py for OrderbookModule.js
+    def simplifyOrderBook(self, order_book_data):
+        orders = {
+            "ETH/USDT:USDT/ETH" : { 'ETH/USDT' : {}, 'USDT/ETH' : {} },
+            "ETH/BNB:BNB/ETH" : { 'ETH/BNB' : {}, 'BNB/ETH' : {} },
+            "ETH/BTC:BTC/ETH" : { 'ETH/BTC' : {}, 'BTC/ETH' : {} },
+            "BNB/BTC:BTC/BNB" : { 'BNB/BTC' : {}, 'BTC/BNB' : {} },
+            "BNB/USDT:USDT/BNB" :{ 'BNB/USDT' : {}, 'USDT/BNB' : {} },
+            "BTC/USDT:USDT/BTC" : { 'BTC/USDT' : {}, 'USDT/BTC' : {} },
+        }
+        for item in order_book_data.items():
+            symbol_orders = order_book_data[item[0]]
+            for order in symbol_orders:
+                if symbol_orders[order]:
+                    i = 0
+                    for agent_key in symbol_orders[order].keys():
+                        value_array = symbol_orders[order][agent_key]
+                        amount = value_array[0]
+                        limit_price = value_array[-1]
+                        exchange_price = 1 / limit_price
+                        temp = {"amount":amount, "limit_price":limit_price, "exchange_price": exchange_price}
+                        orders[item[0]][order][i] = temp
+                        i += 1
+        # Sort the orders ... 
+
+        return orders
+
+
+
